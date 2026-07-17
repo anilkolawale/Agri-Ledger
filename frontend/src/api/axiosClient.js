1bfd0e8f292cd6815414a7d0400d3f398fbcbaf4
anilkolawale <anilkolawale07@gmail.com>
2026-07-18 03:08:37 +0530

    more erro more errors solve 6

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosClient = axios.create({
   baseURL:
     process.env.REACT_APP_API_BASE_URL ||
-    "https://agri-ledger.onrender.com",
+    "https://agri-ledger.onrender.com/api",
 });
 
 // Attach JWT token
